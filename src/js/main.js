0e041480e2720b62724d50e70b5f064cb2a6e751
Moss98 <mohcen66@gmail.com>
2021-03-05 16:49:33 +0100

    Solved bugs with graphic and solved other bugswith capture

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,141 @@
 
-var websites = {};
+
 
 
 chrome.runtime.onMessage.addListener(update);
 
 
 async function init(){
-	websites = await getWebsitesDb();
-	//console.log('eccoci');
-	//console.log(websites);
-	await drawOnIndex(null);
+	var websites = {};
+	var app = await getWebsitesDb();
+	for(let web in app)
+		websites[web] = app[web];
+		await drawOnIndex(websites, website = null);
 }
 
 
 
 async function update(message, sender, sendResponse){
+	var websites = message.websites;
+	var website = message.site;
 	if(message.type == 'updateGraph'){
-		let website = message.args;
-		websites = await getWebsitesDb();
-		await drawOnIndex(website);
+		/*console.log('UPDATEEEEEE');
+		console.log('website aggiornati');
+		console.log(websites);*/
+		await drawOnIndex(websites,website);
 	}
 }
 
 
-function getNodesLinks(){
+async function getNodesLinks(websites){
 	let nodes = [];
 	let links = [];
 	let thirdPartiesWithCookies = new Map();
-	//console.log(websites);
-
+	let cookiesParty = [];
+	var cookiesFirstParty;
+	var cookiesThirdParty;
+	var partyCookies;
+ 	var site = [];
+	var flag = false;
 	for(let website in websites){
-		//console.log('arriva');
-		const site = websites[website];
-	    if (site.thirdPartySites) {
-	    	for(let thirdParty of site.thirdPartySites){
-	    		if(thirdParty.cookies){
-	    			thirdParty.cookies.forEach(element => {element.firstParty = website;}); //DA TROVARE MODO PIù EFFICIENTE
-	    			if(!thirdPartiesWithCookies.has(thirdParty.hostname)){
-	    				thirdPartiesWithCookies.set(thirdParty.hostname,thirdParty.cookies);
+			console.log('SITI');
+			console.log(websites);
+
+
+			//console.log('arriva');
+//			console.log(db.websites.get(hostname));
+			site = await getWebsite(website);
+			console.log('---------SITO WEB CORRENTE---------');
+			console.log(site);
+			cookiesFirstParty = [];
+			cookiesThirdParty = [];
+			partyCookies = [];
+			for(let g of site.cookiesFirstParty){
+				cookiesFirstParty.push(g);
+			}
+
+			for(let g of site.thirdPartySites){
+				cookiesThirdParty.push(g);
+			}
+
+			let obj = {
+				"hostname": site.hostname,
+				"domain" : site.hostname,
+				"cookies": cookiesFirstParty
+			};
+			cookiesParty.push(...cookiesThirdParty.slice());
+			cookiesParty.push(obj);
+
+	    //let thirdParties = site.thirdPartySites;
+	    if (cookiesParty){
+
+	    	for(let party of cookiesParty){
+	    		//for(let i=0; i<site.thirdPartySites.length; i++)
+					partyCookies = party.cookies.slice();
+	    		if(party.cookies.length>0){
+	    			party.cookies.slice().forEach(element => {element.firstParty = website;}); //DA TROVARE MODO PIù EFFICIENTE
+	    			if(!thirdPartiesWithCookies.has(party.hostname)){
+	    				thirdPartiesWithCookies.set(party.hostname,party.cookies.slice()); //TODO: CONSIDERARE CASO NEL QUALE COOKIE VENGONO AGGIUNTI DOPO (ES. DOPO AVER ACCETTARO LE POLICY)
 	    			}else{
-	    				let cookies = thirdPartiesWithCookies.get(thirdParty.hostname);
-	    				//console.log(cookies);
-	    				thirdPartiesWithCookies.set(thirdParty.hostname,cookies);
+	    				let cookies = thirdPartiesWithCookies.get(party.hostname);
+							var current1;
+							var current2;
+							flag = false;
+	    				let isPresent = party.cookies.slice();
 	    				const isLink = cookies.map((element) => {
-	    					for(let cookie of thirdParty.cookies) {
-	    						if(cookie.name == element.name && cookie.value == element.value && cookie.expirationDate == element.expirationDate){
-	    							return {
-		            					'cookie': cookie,
-		            					'website1' : cookie.firstParty,
-		            					'website2' : element.firstParty 
-		       				 		};
-	    						}else{
-	    							/*return {
-		            					'cookie': cookie,
-		            					'shared': false  
-		       				 		};*/
-	    						}
+								flag = false;
+	    					//for(let cookie of thirdParty.cookies){
+	    					for(let i = 0; i<party.cookies.slice().length; i++){
+	    						let cookie = party.cookies.slice()[i];
+		    						if(cookie.name == element.name && cookie.path == element.path && cookie.domain == element.domain && cookie.firstParty != element.firstParty){
+		    							isPresent.splice(i,1);
+											flag = true;
+											current1 = cookie.firstParty;
+											current2 = element.firstParty;
+
+		    						}
+										if(flag){
+											return {
+												//'cookie' : cookie,
+												'website1' : cookie.firstParty,
+												'website2' : element.firstParty
+											};
+										}else {
+											return null;
+										}
+								//	}
 	    					}
 
 	        			});
-	        			links.push(...isLink);
+	        			Array.prototype.push.apply(cookies, party.cookies.slice());
+	        			thirdPartiesWithCookies.set(party.hostname,cookies);
+								var coll = isLink.filter(function (el) {
+  								return el != null;
+								});
+								var uniq = [];
+								uniq = coll.filter(function(item, pos) {
+    							return coll.indexOf(item) == pos;
+								});
+	        			links.push(...coll);
 	    			}
 	    		}
 	    	}
 
 	      }
-	      
-	      
+
+
 	    nodes.push(websites[website]);
 	    links = links.filter(function (el) {
-  				return el != null;
+  				if(el)
+  					return true;
+  				else
+  					return false;
 		});
+		console.log('stampa FINALE');
+		console.log(links);
+		cookiesParty = [];
+
+
 	}
 	return {
       nodes,
@@ -78,19 +143,16 @@ function getNodesLinks(){
     };
 }
 
-
-async function updateDraw(){
-	console.log('UPDATEEEEE');
-
-	websites = await getWebsitesDb();
-	console.log(websites);
-	await drawOnIndex();
-}
-
-async function drawOnIndex(website){
+async function drawOnIndex(websites, website){
 	//disegna tramite p5.js
-	const nodesLinks = getNodesLinks();
-	initSketch(nodesLinks.nodes,nodesLinks.links,website);
+	let site;
+	const nodesLinks = await getNodesLinks(websites);
+	if(website){
+		site = website;
+		initSketch(nodesLinks.nodes,nodesLinks.links,site);
+	}else{
+		initSketch(nodesLinks.nodes,nodesLinks.links);
+	}
 }
 
 window.onload = () => {
--- src/js/store.js
+++ src/js/store.js
@@ -16,15 +16,28 @@
 		return await db.websites.get(hostname);
 	}
 
+	async function getWebsite(hostname){
+		return await db.websites.get(hostname);
+	}
+
+	async function getrows(){
+  	return db.websites.where('hostname').notEqual(' ').toArray();
+	}
+
 	async function getWebsitesDb(){
-		 const websites = await db.websites.filter((website) => {
-	      	return website.firstParty;
-	    }).toArray();
-	    const output = {};
-	    for (const website of websites) {
-	      output[website.hostname]
-	        = website;
+			var prova = await getrows();
+			prova = prova.filter((website) => {
+				if(website.cookiesFirstParty)
+					return website;
+			});
+	    var output = {};
+	    for (let sito of	prova) {
+				console.log(sito);
+	      output[sito.hostname] = sito;
 	    }
+			console.log('OUTPUT');
+ 		 	for(let pp in output)
+				console.log(output[pp]);
     	return output;
   	}
 
@@ -43,12 +56,14 @@
 
 	async function writeDb(website) {
     	for (const key in website) {
-      		website[key] = convertBooleans(key, website[key]);
+      		website[key] = convertBooleans(website[key]);
     	}
+			console.log('SITO DA AGGIUNGERE: ');
+			console.log(website);
     	return await db.websites.put(website);
   	}
 
-  	function convertBooleans(key,element){
+  	function convertBooleans(element){
   		if (element === true) {
        		element = 1;
       	}
@@ -60,8 +75,10 @@
 
   	async function updateDb(hostname,website){
   		for (const key in website) {
-      		website[key] = convertBooleans(key, website[key]);
+      		website[key] = convertBooleans(website[key]);
     	}
+			console.log('SITO DA UPDATARE: ---->');
+			console.log(website);
     	return await db.websites.update(hostname,website);
   	}
 
@@ -75,8 +92,8 @@
   	}
 
 
-  	async function storeThirdParty(hostname,party){
-  		var website = {};
+  async function storeThirdParty(hostname,party){
+  	var website = {};
 		notPresent = await isNotPresent(hostname);
 		if(!notPresent){
 			website = await getWebsite(hostname);
@@ -91,21 +108,25 @@
 		}
 		let obj = {
 			"hostname": party.target,
+			"domain" : (party.cookiesThirdParty.length>0) ? party.cookiesThirdParty[0].domain : '',
 			"cookies": (party.cookiesThirdParty.length>0) ? party.cookiesThirdParty : ''
 		};
 		if(!await isDuplicate(website['thirdPartySites'],party.target)){
 			website['thirdPartySites'].push(obj);
 			await updateDb(hostname,website);
+			console.log('DB AGGIORNATO: --->');
+			console.log(db.websites.get(hostname));
 		}
 		return website;
   	}
 
-	  	async function storeFirstParty(hostname,party){
-	  		var website = {};
+	  async function storeFirstParty(hostname,party){
+	  	var website = {};
 			notPresent = await isNotPresent(hostname);
 			if(!notPresent){
 				website = await getWebsite(hostname);
 			}
+			console.log('PROVA NOTPRESENT: -->'+ notPresent);
 			if(notPresent || website['firstPartyInserted'] == 0){ // se non è presente oppure se è stato aggiunto da terze parti
 				for(let key in party){
 					if(key === 'cookiesFirstParty'){
@@ -117,9 +138,11 @@
 					website[key] = party[key];
 				}
 				if(notPresent){
+					console.log('CIAOOOOOOOOOOOOOOOOOOOOOAAA');
 					await writeDb(website);
 				}
 				if(website['firstPartyInserted'] == 0){
+					website['firstPartyInserted'] = 1;
 					await updateDb(hostname,website);
 				}
 			}else{
@@ -135,29 +158,32 @@
 	  	}
 
 
-	function updateGraph(...args) {
+	async function updateGraph(args2, args) {
+			const site = args2;
+			const websites = args;
     	chrome.runtime.sendMessage({
       		type: 'updateGraph',
-      		args
+      		websites,
+					site
     	});
   	}
 
 	async function storeParty(hostname, party){
 		var website = {};
-		console.log('party: '+party.firstParty);
+		//console.log('party: '+party.firstParty);
 		if(party !== undefined){
 			switch(party.firstParty){
 				case true:
 					website = await storeFirstParty(hostname,party);
-					console.log('ciaoooo');
-					updateGraph(website);
-					console.log('funziona qua');
+					let prova = await getWebsitesDb();
+					await updateGraph(website,prova);
+					//updateGraph(website);
 				break;
-				case false: 
+				case false:
 					website = await storeThirdParty(hostname,party);
 
 				break;
-					
+
 			}
 		}
 		//console.log(website);
